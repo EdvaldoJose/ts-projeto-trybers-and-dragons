07b7c4b614172b9fa9c13fbd456c109e7abd156d
Edvaldo Jose <edvaldojose003@gmail.com>
2023-02-17 21:09:14 -0300

    Requisito 10 concluido

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -8,14 +8,3 @@ export default interface Fighter extends SimpleFighter {
   special?(enemy: SimpleFighter): void,
   levelUp(): void,
 }
-
-// export default interface Fighter extends SimpleFighter {
-//   // lifePoints: number;
-//   // strength: number;
-//   defense: number;
-//   energy?: Energy;
-//   // attack(enemy: Fighter): void;
-//   special?(enemy: SimpleFighter): void;
-//   levelUp(): void;
-//   // receiveDamage(attackPoints: number): number;
-// }
